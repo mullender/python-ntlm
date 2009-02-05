da1da0eb1684833f133238aa913325a5993f3061
duncancbennett <duncancbennett@fb9420a8-c688-11dd-8ca8-e3ff79f713b6>
2009-02-05 08:52:54 +0000

    Added extra tests as well as skeleton code for NTLMHandlers

---
+++ branches/clientserver/ntlm/ntlmhandler.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+import ntlm
+from ntlm2 import NTLM_FLAGS
+import des
+import hashlib
+import hmac
+
+def desl(k, d):
+    """Helper function which implements "Data Encryption Standard Long" algorithm.
+       "k" should be a 16 byte value which gets padded by 5 bytes. "d" should be an 8 byte value."""
+    # padding with zeros to make the hash 21 bytes long
+    password_hash = k + '\0' * (21 - len(k))
+    res = ''
+    dobj = des.DES(password_hash[0:7])
+    res = res + dobj.encrypt(d[0:8])
+
+    dobj = des.DES(password_hash[7:14])
+    res = res + dobj.encrypt(d[0:8])
+
+    dobj = des.DES(password_hash[14:21])
+    res = res + dobj.encrypt(d[0:8])
+    return res
+
+class ResponseData:
+    def __init__(self, ResponseKeyNT, ResponseKeyLM, NTChallengeResponse=None, LmChallengeResponse=None, SessionBaseKey=None):
+	self.ResponseKeyNT = ResponseKeyNT
+	self.ResponseKeyLM = ResponseKeyLM
+	self.NTChallengeResponse = NTChallengeResponse
+	self.LmChallengeResponse = LmChallengeResponse
+	self.SessionBaseKey = SessionBaseKey
+
+#-----------------------------------------------------------------------------------------------
+# BaseHandler
+#-----------------------------------------------------------------------------------------------
+
+class BaseHandler(object):
+    """Base class for a set of NTLM helpers which encapsulate the logic used to encode NTLM messages.
+       This should make it easy to switch between versions of NTLM."""
+
+    unicode = 'utf-16le'
+
+    def __init__(self, encoding='utf-16le'):
+	self.encoding = encoding
+
+    def create_LM_hashed_password(self, password, user, domain):
+	"""Returns an LM hashed password based on the NTLM version implementation.
+	   user and domain are required for v2 and can just be ignored for version 1"""
+	raise NotImplementedError("%s.%s needs a \"create_LM_hashed_password\" function"%(self.__class__.__module__, self.__class__.__name__))
+
+    def create_NT_hashed_password(self, password, user, domain):
+	"""Returns a NT hashed password based on the NTLM version implementation.
+	   user and domain are required for v2 and can just be ignored for version 1"""
+	raise NotImplementedError("%s.%s needs a \"create_NT_hashed_password\" function"%(self.__class__.__module__, self.__class__.__name__))
+
+    def compute_response(self, NegFlg, password, user, domain, ServerChallenge, ClientChallenge, Time, ServerName):
+	"""Returns NTChallengeResponse and LmChallengeResponse values based on the NTLM version implementation.
+	   Where either of these return values is none, its xChallengeResponseLen, xChallengeResponseMaxLen and
+	   xChallengeResponseBufferOffset values should be set to 0 in the calling scope.
+	   user and domain are required for v2 and can just be ignored for version 1"""
+	raise NotImplementedError("%s.%s needs a \"compute_response\" function"%(self.__class__.__module__, self.__class__.__name__))
+
+    def create_negotiate_message(self):
+	"""Still need to decide what arguments this should take"""
+
+    def create_challenge_message(self):
+	"""Still need to decide what arguments this should take"""
+
+    def create_authenticate_message(self):
+	"""Still need to decide what arguments this should take"""
+
+#-----------------------------------------------------------------------------------------------
+# NTLMHandler_v1
+#-----------------------------------------------------------------------------------------------
+
+class NTLMHandler_v1(BaseHandler):
+
+    def create_LM_hashed_password(self, password, user, domain):
+	return ntlm.create_LM_hashed_password_v1(password)
+
+    def create_NT_hashed_password(self, password, user, domain):
+	return hashlib.new('md4', password.encode(self.unicode)).digest()
+
+    def compute_response(self, NegFlg, password, user, domain, ServerChallenge, ClientChallenge, Time, ServerName):
+	ResponseKeyNT = self.create_NT_hashed_password(password, user, domain)
+	ResponseKeyLM = self.create_LM_hashed_password(password, user, domain)
+	NTChallengeResponse=None
+	LmChallengeResponse=None
+	#TODO - the string below contains the logic for LM Authentication but it is not clear whether NTLM_FLAGS.NTLMSSP_NEGOTIATE_LM_KEY
+	#means that LM Authentication is being used. Determine whether or not this code is correct
+	"""if NegFlg & NTLM_FLAGS.NTLMSSP_NEGOTIATE_LM_KEY and not NegFlg & NTLM_FLAGS.NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY:
+	    #Leave NTChallengeResponse=None
+	    #TODO - make sure that NtChallengeResponseLen, NtChallengeResponseMaxLen and NtChallengeResponseBufferOffset are set to 0
+	    #by the calling function
+	    LmChallengeResponse = desl(ResponseKeyLM, ServerChallenge)
+	elif NegFlg & NTLM_FLAGS.NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY:"""
+	if NegFlg & NTLM_FLAGS.NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY:
+	    challenge = hashlib.md5(ServerChallenge+ClientChallenge).digest()
+	    NTChallengeResponse = desl(ResponseKeyNT, challenge[0:8])
+	    LmChallengeResponse = ClientChallenge + '\0' * 16
+	else:
+	    NTChallengeResponse = desl(ResponseKeyNT, ServerChallenge)
+
+	    if NegFlg & NTLM_FLAGS.NTLMSSP_NEGOTIATE_NT_ONLY:
+		LmChallengeResponse = NTChallengeResponse
+	    else:
+		LmChallengeResponse = desl(ResponseKeyLM, ServerChallenge)
+
+	return ResponseData(ResponseKeyNT,
+			    ResponseKeyLM,
+			    NTChallengeResponse,
+			    LmChallengeResponse,
+			    hashlib.new('md4', ResponseKeyNT).digest())
+
+#-----------------------------------------------------------------------------------------------
+# NTLMHandler_v2
+#-----------------------------------------------------------------------------------------------
+
+class NTLMHandler_v2(BaseHandler):
+    def create_LM_hashed_password(self, password, user, domain):
+	return self.create_NT_hashed_password(password, user, domain)
+
+    def create_NT_hashed_password(self, password, user, domain):
+	digest = hashlib.new('md4', password.encode(self.unicode)).digest()
+	return hmac.new(digest, (user.upper()+domain).encode(self.encoding)).digest()
+
+    def compute_response(self, NegFlg, password, user, domain, ServerChallenge, ClientChallenge, Time, ServerName):
+	ResponseKeyNT = self.create_NT_hashed_password(password, user, domain)
+	ResponseKeyLM = self.create_LM_hashed_password(password, user, domain)
+	NTChallengeResponse=None
+	LmChallengeResponse=None
+	
+	#TODO get proper values for the hardcoded values Responserversion and HiResponserversion
+	HiResponserversion = Responserversion = "\x01"
+	temp = self._temp(Responserversion, HiResponserversion, Time, ClientChallenge, ServerName)
+
+	NTProofStr = self._nt_proof_str(ResponseKeyNT, ServerChallenge, temp)
+	SessionBaseKey = hmac.new(ResponseKeyNT, NTProofStr).digest()
+
+	NTChallengeResponse = NTProofStr + temp
+	LmChallengeResponse = hmac.new(ResponseKeyLM, ServerChallenge + ClientChallenge).digest() + ClientChallenge
+
+
+	return ResponseData(ResponseKeyNT,
+			    ResponseKeyLM,
+			    NTChallengeResponse,
+			    LmChallengeResponse,
+			    SessionBaseKey)
+
+    def _nt_proof_str(self, ResponseKeyNT, ServerChallenge, temp):
+	return hmac.new(ResponseKeyNT, ServerChallenge+temp).digest()
+
+    def _temp(self, Responserversion, HiResponserversion, Time, ClientChallenge, ServerName):
+	return Responserversion + HiResponserversion + '\x00'*6 + Time + ClientChallenge + '\x00'*4 + ServerName + '\x00'*4
+
