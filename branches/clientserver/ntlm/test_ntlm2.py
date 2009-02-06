d2e301b2b3923f2b3abc0a75b9256bd2cfb1208e
duncancbennett <duncancbennett@fb9420a8-c688-11dd-8ca8-e3ff79f713b6>
2009-02-06 13:54:00 +0000

    more tests

--- branches/clientserver/ntlm/test_ntlm2.py
+++ branches/clientserver/ntlm/test_ntlm2.py
@@ -97,7 +97,11 @@ class TestNTLMClient(object):
         #assert responsedata.NTChallengeResponse == HexToByte("68 cd 0a b8 51 e5 1c 96 aa bc 92 7b eb ef 6a 1c")
         assert responsedata.LmChallengeResponse == HexToByte("86c35097 ac9cec10 2554764a 57cccc19 aaaaaaaa aaaaaaaa")
 
-    def test_parse_v1_negotiate_message(self):
+    # -------------------------------------------------------------------------------------------------------------
+    # Negotiate Message Tests
+    # -------------------------------------------------------------------------------------------------------------
+
+    def test_parse_simplest_negotiate_message(self):
         #Version 1 negotiate message test
 
         #Test simplest possible negotiate message
@@ -114,13 +118,16 @@ class TestNTLMClient(object):
         version = negotiate_message.get_version_field()
         assert version is None
 
-        expectedflags = NTLM_FLAGS.NTLMSSP_NEGOTIATE_UNICODE | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM | NTLM_FLAGS.NTLMSSP_REQUEST_TARGET | NTLM_FLAGS.NTLMSSP_NEGOTIATE_NTLM | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED
+    def test_parse_normal_negotiate_message(self):
+        #Version 1 negotiate message test
 
         #Test negotiate message with no version information
         message = HexToByte("4e544c4d535350000100000007320000060006002b0000000b000b0020000000574f524b53544154494f4e444f4d41494e")
         f = StringIO.StringIO(message)
         negotiate_message = ntlm2.NTLMMessage.read(f)
         negotiate_message.verify()
+        expectedflags = NTLM_FLAGS.NTLMSSP_NEGOTIATE_UNICODE | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM | NTLM_FLAGS.NTLMSSP_REQUEST_TARGET | NTLM_FLAGS.NTLMSSP_NEGOTIATE_NTLM | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED
+
         assert negotiate_message.Header.MessageType == ntlm2.NTLM_MESSAGE_TYPE.NtLmNegotiate.const
         assert negotiate_message.get_string_field("Workstation") == "WORKSTATION"
         assert negotiate_message.get_string_field("DomainName") == "DOMAIN"
@@ -129,6 +136,7 @@ class TestNTLMClient(object):
         version = negotiate_message.get_version_field()
         assert version is None
 
+    def test_parse_full_negotiate_message(self):
         #Test full negotiate message
         #Example taken from http://davenport.sourceforge.net/ntlm.html#theType1Message
         message = HexToByte("4e544c4d53535000010000000732000206000600330000000b000b0028000000050093080000000f574f524b53544154494f4e444f4d41494e")
@@ -136,12 +144,14 @@ class TestNTLMClient(object):
         f = StringIO.StringIO(message)
         negotiate_message = ntlm2.NTLMMessage.read(f)
         negotiate_message.verify()
+        expectedflags = NTLM_FLAGS.NTLMSSP_NEGOTIATE_VERSION | NTLM_FLAGS.NTLMSSP_NEGOTIATE_UNICODE | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM | NTLM_FLAGS.NTLMSSP_REQUEST_TARGET | NTLM_FLAGS.NTLMSSP_NEGOTIATE_NTLM | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED
+
         assert negotiate_message.Header.MessageType == ntlm2.NTLM_MESSAGE_TYPE.NtLmNegotiate.const
         assert negotiate_message.get_string_field("Workstation") == "WORKSTATION"
         assert negotiate_message.get_string_field("DomainName") == "DOMAIN"
         assert negotiate_message.get_string_fields() == {"DomainName": "DOMAIN", "Workstation": "WORKSTATION"}
 
-        assert negotiate_message.MessageFields.NegotiateFlags == NTLM_FLAGS.NTLMSSP_NEGOTIATE_VERSION | expectedflags
+        assert negotiate_message.MessageFields.NegotiateFlags == expectedflags
 
         version = negotiate_message.get_version_field()
         assert version is not None
@@ -150,7 +160,7 @@ class TestNTLMClient(object):
         assert version.ProductBuild == 2195
         assert version.NTLMRevisionCurrent == 0xf
 
-    def test_manually_create_negotiate_message(self):
+    def test_manually_create_simple_negotiate_message(self):
         """Tests the new method of creating ntlm negotiate messages"""
         negotiate_message = ntlm2.NTLMMessage()
         negotiate_message.Header.Signature = ntlm2.NTLM_PROTOCOL_SIGNATURE
@@ -165,7 +175,11 @@ class TestNTLMClient(object):
         #This test fails because blank values still get encoded. So the message is still correct but there are unneeded trailing zeros
         assert negotiate_bytes == HexToByte("4e544c4d535350000100000002020000")
 
+    def test_manually_create_normal_negotiate_message(self):
         flags = NTLM_FLAGS.NTLMSSP_NEGOTIATE_UNICODE | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM | NTLM_FLAGS.NTLMSSP_REQUEST_TARGET | NTLM_FLAGS.NTLMSSP_NEGOTIATE_NTLM | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED
+        negotiate_message = ntlm2.NTLMMessage()
+        negotiate_message.Header.Signature = ntlm2.NTLM_PROTOCOL_SIGNATURE
+        negotiate_message.Header.MessageType = ntlm2.NTLM_MESSAGE_TYPE.NtLmNegotiate.const
         negotiate_message.set_negotiate_flags(flags)
         negotiate_message.set_string_field("DomainName", "DOMAIN")
         negotiate_message.set_string_field("Workstation", "WORKSTATION")
@@ -175,7 +189,11 @@ class TestNTLMClient(object):
         negotiate_bytes = base64.b64decode(negotiate_b64)
         assert negotiate_bytes == HexToByte("4e544c4d535350000100000007320000060006002b0000000b000b0020000000574f524b53544154494f4e444f4d41494e")
 
+    def test_manually_create_full_negotiate_message(self):
         flags = NTLM_FLAGS.NTLMSSP_NEGOTIATE_VERSION | NTLM_FLAGS.NTLMSSP_NEGOTIATE_UNICODE | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM | NTLM_FLAGS.NTLMSSP_REQUEST_TARGET | NTLM_FLAGS.NTLMSSP_NEGOTIATE_NTLM | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_DOMAIN_SUPPLIED | NTLM_FLAGS.NTLMSSP_NEGOTIATE_OEM_WORKSTATION_SUPPLIED
+        negotiate_message = ntlm2.NTLMMessage()
+        negotiate_message.Header.Signature = ntlm2.NTLM_PROTOCOL_SIGNATURE
+        negotiate_message.Header.MessageType = ntlm2.NTLM_MESSAGE_TYPE.NtLmNegotiate.const
         negotiate_message.set_negotiate_flags(flags)
         version = negotiate_message.get_version_field()
         version.ProductMajorVersion = 5
@@ -204,6 +222,40 @@ class TestNTLMClient(object):
         negotiate_bytes = base64.b64decode(negotiate_b64)
         assert negotiate_bytes == HexToByte("4e544c4d535350000100000007b20000060006002b0000000b000b0020000000574f524b53544154494f4e444f4d41494e")
 
+
+    # -------------------------------------------------------------------------------------------------------------
+    # Challenge Message Tests
+    # -------------------------------------------------------------------------------------------------------------
+
+    def test_parse_simple_challenge(self):
+        """Tests parsing ntlm challenge messages"""
+        callenge = HexToByte("4e544c4d53535000020000000000000000000000020200000123456789abcdef")
+        f = StringIO.StringIO(callenge)
+        challenge_message = ntlm2.NTLMMessage.read(f)
+        assert challenge_message.Header.MessageType == ntlm2.NTLM_MESSAGE_TYPE.NtLmChallenge.const
+        assert challenge_message.get_string_fields() == {"TargetName": "", "TargetInfo": ""}
+        challenge_fields = challenge_message.MessageFields
+        assert isinstance(challenge_fields, ntlm2.NTLMMessageChallengeFields)
+        assert challenge_fields.ServerChallenge[0:8] == [ord(c) for c in HexToByte("0123456789abcdef")]
+        assert challenge_fields.NegotiateFlags == 0x0202
+
+    def test_parse_full_challenge(self):
+        """Tests parsing ntlm challenge messages"""
+        callenge = HexToByte("4e544c4d53535000020000000c000c0030000000010281000123456789abcdef0000000000000000620062003c00000044004f004d00410049004e0002000c0044004f004d00410049004e0001000c005300450052005600450052000400140064006f006d00610069006e002e0063006f006d00030022007300650072007600650072002e0064006f006d00610069006e002e0063006f006d0000000000")
+        f = StringIO.StringIO(callenge)
+        challenge_message = ntlm2.NTLMMessage.read(f)
+        assert challenge_message.Header.MessageType == ntlm2.NTLM_MESSAGE_TYPE.NtLmChallenge.const
+        #TargetName MUST be expressed in the negotiated character set [MS-NLMP] page 21.
+        #If a TargetInfo AV_PAIR Value is textual, it MUST be encoded in Unicode irrespective of what character set was negotiated [MS-NLMP] page 21.
+        tname = "DOMAIN".encode("utf-16le")
+        tinfo = HexToByte("02000c0044004f004d00410049004e0001000c005300450052005600450052000400140064006f006d00610069006e002e0063006f006d00030022007300650072007600650072002e0064006f006d00610069006e002e0063006f006d0000000000")
+        assert challenge_message.get_string_field("TargetName") == tname
+        assert challenge_message.get_string_field("TargetInfo") == tinfo
+        assert challenge_message.get_string_fields() == {"TargetName": tname, "TargetInfo": tinfo}
+        challenge_fields = challenge_message.MessageFields
+        assert isinstance(challenge_fields, ntlm2.NTLMMessageChallengeFields)
+        assert challenge_fields.ServerChallenge[0:8] == [ord(c) for c in HexToByte("0123456789abcdef")]
+        assert challenge_fields.NegotiateFlags ==  0x00000001 | 0x00000200 |0x00010000 | 0x00800000
 
 #TODO - Setup tests, which make sure that flags are set automatically as per the [MS-NLMP] specification
 #     - When certain flags are set, the spec demands that other flags are set/not set in each of the message types
