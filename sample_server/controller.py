11b8d30e850d56ae4e9d94288fecd2fbdd8ba47a
davidf@sjsoft.com <davidf@sjsoft.com@fb9420a8-c688-11dd-8ca8-e3ff79f713b6>
2009-03-04 14:46:34 +0000

    Added option to set hostname to listen on

--- sample_server/controller.py
+++ sample_server/controller.py
@@ -39,6 +39,8 @@ def main(options):
         'tools.trailing_slash.on': True,
         'tools.staticdir.root': os.path.abspath(os.path.dirname(__file__)),
     })
+    if options.host:
+        cherrypy.config["server.socket_host"] = options.host
 
     cherrypy.quickstart(Root(data), '/', {
         '/static': {
@@ -54,15 +56,14 @@ if __name__ == '__main__':
     import sys
     import optparse
     optparser = optparse.OptionParser()
-    optparser.add_option("", "--loglevel",
-                    type="string", dest="loglevel", default="warn",
+    optparser.add_option("", "--loglevel", type="string", dest="loglevel", default="warn",
                     help="Logging level. Options are: notset, debug, info, warn, error, fatal. [warn]")
-    optparser.add_option("-1", "--version1",
-                    action="store_const", dest="ntlm_version", default=1, const=1,
+    optparser.add_option("-1", "--version1", action="store_const", dest="ntlm_version", default=1, const=1,
                     help="Require NTLM Version 1")
-    optparser.add_option("-2", "--version2",
-                    action="store_const", dest="ntlm_version", const=2,
+    optparser.add_option("-2", "--version2", action="store_const", dest="ntlm_version", const=2,
                     help="Require NTLM Version 2")
+    optparser.add_option("", "--host", type="string", dest="host", default=None,
+                    help="Listen on the given host name/IP address")
     options, arguments = optparser.parse_args()
     if arguments:
         optparser.error("Unexpected arguments %s" % (" ".join(arguments)))
