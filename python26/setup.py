ecf4c968d505dd32e6a4c6335aa3892a7462f4c2
dholth <dholth@fb9420a8-c688-11dd-8ca8-e3ff79f713b6>
2011-02-17 18:29:55 +0000

    update description. no longer pin hashlib dependency per python-ntlm-fixed (only required for Python < 2.5)

--- python26/setup.py
+++ python26/setup.py
@@ -7,17 +7,27 @@ SRC_FOLDER = "src"
 ENTRY_POINTS = { "console_scripts":[ "ntlm_example_simple=ntlm_examples.simple:main",
                                      "ntlm_example_extended=ntlm_examples.extended:main",] }
 
-DEPENDANCIES = []
+DEPENDENCIES = []
 
 if sys.version_info < ( 2,5 ):
-    DEPENDANCIES.append( "hashlib==20060408a" )
+    DEPENDENCIES.append( "hashlib" )
     
 setup(name='python-ntlm',
       version='1.0',
       description='Python library that provides NTLM support, including an authentication handler for urllib2.',
+      long_description="""
+      This package allows Python clients running on any operating
+      system to provide NTLM authentication to a supporting server.
+      
+      python-ntlm is probably most useful on platforms that are not
+      Windows, since on Windows it is possible to take advantage of
+      platform-specific NTLM support.""",
       author='Matthijs Mullender',
+      maintainer='Daniel Holth',
+      maintainer_email='dholth@gmail.com',
+      url="http://code.google.com/p/python-ntlm",
       packages=["ntlm",],
       zip_safe=False,
       entry_points = ENTRY_POINTS,
-      install_requires = DEPENDANCIES,
+      install_requires = DEPENDENCIES,
       )
