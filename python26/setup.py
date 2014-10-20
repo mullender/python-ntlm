0d948fd0cf4b245587bd13caeefd71a0c2511d43
deronnax@gmail.com <deronnax@gmail.com@fb9420a8-c688-11dd-8ca8-e3ff79f713b6>
2014-10-20 14:48:12 +0000

    add pypi python2 classifiers to setup.py

--- python26/setup.py
+++ python26/setup.py
@@ -29,4 +29,12 @@ setup(name='python-ntlm',
       packages=["ntlm",],
       zip_safe=False,
       entry_points = ENTRY_POINTS,
+      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ],
       )
